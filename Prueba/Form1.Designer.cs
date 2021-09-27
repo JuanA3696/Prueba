3cd1fca7c6f65aa79c736b7c3ce994cd1550460b
Edwin Fernando Ortega Diaz <efortega62@misena.edu.co>
2021-09-27 09:36:12 -0500

    pal crud

--- Prueba/Form1.Designer.cs
+++ Prueba/Form1.Designer.cs
@@ -30,48 +30,113 @@ namespace Prueba
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnB = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnDrop = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.dtgDATO = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDATO)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(66, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 29);
+            this.label1.Size = new System.Drawing.Size(73, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "HOLA";
+            this.label1.Text = "name";
             // 
-            // btnB
+            // btnDrop
             // 
-            this.btnB.Location = new System.Drawing.Point(68, 108);
-            this.btnB.Name = "btnB";
-            this.btnB.Size = new System.Drawing.Size(134, 63);
-            this.btnB.TabIndex = 1;
-            this.btnB.Text = "Go";
-            this.btnB.UseVisualStyleBackColor = true;
+            this.btnDrop.Location = new System.Drawing.Point(571, 108);
+            this.btnDrop.Name = "btnDrop";
+            this.btnDrop.Size = new System.Drawing.Size(134, 63);
+            this.btnDrop.TabIndex = 1;
+            this.btnDrop.Text = "Drop";
+            this.btnDrop.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnUpdate
             // 
-            this.button1.Location = new System.Drawing.Point(317, 108);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 63);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Go1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnUpdate.Location = new System.Drawing.Point(571, 39);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(134, 63);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(418, 39);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(134, 63);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(418, 108);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(134, 63);
+            this.btnInsertar.TabIndex = 4;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "lastName";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(71, 53);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(150, 26);
+            this.txtname.TabIndex = 7;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(71, 126);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(150, 26);
+            this.txtlastname.TabIndex = 8;
+            // 
+            // dtgDATO
+            // 
+            this.dtgDATO.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDATO.Location = new System.Drawing.Point(71, 194);
+            this.dtgDATO.Name = "dtgDATO";
+            this.dtgDATO.RowHeadersWidth = 62;
+            this.dtgDATO.Size = new System.Drawing.Size(657, 209);
+            this.dtgDATO.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.dtgDATO);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDrop);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDATO)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,8 +145,14 @@ namespace Prueba
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnB;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDrop;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.DataGridView dtgDATO;
     }
 }
 
